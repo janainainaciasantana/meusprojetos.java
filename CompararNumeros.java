05e55886cdeb63dcf34833def155982ec252f057
janainainaciasantana <eebmp.jiso@educacao.arcos.mg.gov.br>
2024-06-03 18:28:02 -0300

    05-24

--- CompararNumeros.java
+++ CompararNumeros.java
@@ -1,20 +1,24 @@
-import java.util.CompararNumeros
+import java.util.Scanner;
 public class CompararNumeros {
     public static void main(String[] args) {
-        int valorA;
-        int valorB;
-        if (valorA==valorB){
-            System.out.println(x:"A é ");
-        }
-    if (valorA >=valorB){
-        System.out.println(x"A é maior ou igual que B);
+        int num1;
+        int num2;
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Digite o primeiro numero");
+        num1= scanner.nextInt();
+        System.out.print("Digite o segundo numero");
+        num2= scanner.nextInt();
+        if(num1> num2){
+            System.out.print("numero1 é maior que numero2");
 
-    }
-    if (valorA < valorB){
-        System.out.println(x:"A é menor que B");
-    }
-    if (valorA<=valorB) {
-        System.out.println(x:"A é menor ou igual B");
-    }
-    }
+        }else if(num1<num2){
+            System.out.print("numero1 é menor que numero2");
+
+        }else if(num1== num2){
+            System.out.print("Sao iguais");
+
+        }
+        )scanner.close();     
+
+    }  
 }
