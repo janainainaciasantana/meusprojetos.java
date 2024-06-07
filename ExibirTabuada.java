38927961d31042c53e5226af1cf9f47ac879fce5
janainainaciasantana <eebmp.jiso@educacao.arcos.mg.gov.br>
2024-06-06 22:22:22 -0300

    Avaliação

---
+++ ExibirTabuada.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class main ExibirTabuada{
+    public static void main(String[]args);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite um numero para calcular a tabuada");
+    int numero;
+    for(int i =1; i <=10; i++){
+        System.out.println("numero + " x " + i + " = " + (numero * i));
+        scanner.close();
+    }
+}
