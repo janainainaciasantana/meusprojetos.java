191c08b5d94d7380629b5d6bd616dbadec6046dd
janainainaciasantana <eebmp.jiso@educacao.arcos.mg.gov.br>
2024-06-20 20:54:36 -0300

    Create TestarPOPUP.java

---
+++ TestarPOPUP.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TestarPOPUP {
+    public static void main (String [] args){
+        JOptionPane.showMessageDialog(null, "Aconteceu um erro!", "Mensagem de erro", JOptionPane.ERROR_MESSAGE);
+    
+        String nome;
+        nome = JOptionPane.showInputDialog("Digite seu nome:");
+        JOptionPane.showMessageDialog(null, "Bem - vindo, " + nome);
+ 
+}
+
+}
