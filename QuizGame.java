ef2fa4a927b154041e8dbabfccf649596bd61ec9
janainainaciasantana <eebmp.jiso@educacao.arcos.mg.gov.br>
2024-06-19 21:54:18 -0300

    Create QuizGame.java

---
+++ QuizGame.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class QuizGame{
+      public static void main (String[]{
+     limiteRespostas++;
+      if (limiteRespostas==10){
+        for (int indice = 0 indice < gabarito.length; indice++){
+            if(gabarito[indice] == respostas[indice] {
+              pontuacao = pontuacao + 1;
+
+        }
+      System.out.printtln("=================");
+      System.out.println("Fim de jogo - Sua pontuacao foi" + pontuacao);
+      break;
+
+    }
+if (opcaoUsuario == 2) {
+    System.out.println("==================");
+    system.out.println("Saindo do Quiz Game em 3...2...1...-Ate a proxima!");
+}
+ scan.close();   
+}
+
+
+    
